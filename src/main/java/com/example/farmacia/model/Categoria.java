a652339d67e136d07ee206381537b75f10bf212d
Eduardo Nogueira <eduardo7_@hotmail.com>
2023-10-19 09:30:39 -0300

    Criação CRUD de Categoria

---
+++ src/main/java/com/example/farmacia/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.example.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O Atributo Descrição é obrigatório")
+    private String categoria;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
